c00f65831a576f73772f8c2f92f03e81279d3bfa
zehua.wang <zehua.wang@grabtaxi.com>
2021-09-15 01:12:30 +0800

    align with 2021 lab1 tests on 2 mrapps

--- src/main/test-mr.sh
+++ src/main/test-mr.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/local/bin bash
 
 #
 # basic map-reduce test
@@ -20,6 +20,8 @@ rm -f mr-*
 (cd ../../mrapps && go build $RACE -buildmode=plugin indexer.go) || exit 1
 (cd ../../mrapps && go build $RACE -buildmode=plugin mtiming.go) || exit 1
 (cd ../../mrapps && go build $RACE -buildmode=plugin rtiming.go) || exit 1
+(cd ../../mrapps && go build $RACE -buildmode=plugin jobcount.go) || exit 1
+(cd ../../mrapps && go build $RACE -buildmode=plugin early_exit.go) || exit 1
 (cd ../../mrapps && go build $RACE -buildmode=plugin crash.go) || exit 1
 (cd ../../mrapps && go build $RACE -buildmode=plugin nocrash.go) || exit 1
 (cd .. && go build $RACE mrmaster.go) || exit 1
@@ -151,6 +153,72 @@ fi
 
 wait ; wait
 
+#########################################################
+echo '***' Starting job count test.
+
+rm -f mr-*
+
+timeout -k 2s 180s ../mrmaster ../pg*txt &
+sleep 1
+
+timeout -k 2s 180s ../mrworker ../../mrapps/jobcount.so &
+timeout -k 2s 180s ../mrworker ../../mrapps/jobcount.so
+timeout -k 2s 180s ../mrworker ../../mrapps/jobcount.so &
+timeout -k 2s 180s ../mrworker ../../mrapps/jobcount.so
+
+NT=`cat mr-out* | awk '{print $2}'`
+if [ "$NT" -ne "8" ]
+then
+  echo '---' map jobs ran incorrect number of times "($NT != 8)"
+  echo '---' job count test: FAIL
+  failed_any=1
+else
+  echo '---' job count test: PASS
+fi
+
+wait
+
+#########################################################
+# test whether any worker or coordinator exits before the
+# task has completed (i.e., all output files have been finalized)
+rm -f mr-*
+
+echo '***' Starting early exit test.
+
+timeout -k 2s 180s ../mrmaster ../pg*txt &
+
+# give the coordinator time to create the sockets.
+sleep 1
+
+# start multiple workers.
+timeout -k 2s 180s ../mrworker ../../mrapps/early_exit.so &
+timeout -k 2s 180s ../mrworker ../../mrapps/early_exit.so &
+timeout -k 2s 180s ../mrworker ../../mrapps/early_exit.so &
+
+# wait for any of the coord or workers to exit
+# `jobs` ensures that any completed old processes from other tests
+# are not waited upon
+jobs &> /dev/null
+wait -n
+
+# a process has exited. this means that the output should be finalized
+# otherwise, either a worker or the coordinator exited early
+sort mr-out* | grep . > mr-wc-all-initial
+
+# wait for remaining workers and coordinator to exit.
+wait
+
+# compare initial and final outputs
+sort mr-out* | grep . > mr-wc-all-final
+if cmp mr-wc-all-final mr-wc-all-initial
+then
+  echo '---' early exit test: PASS
+else
+  echo '---' output changed after first worker exited
+  echo '---' early exit test: FAIL
+  failed_any=1
+fi
+rm -f mr-*
 
 # generate the correct output
 ../mrsequential ../../mrapps/nocrash.so ../pg*txt || exit 1
